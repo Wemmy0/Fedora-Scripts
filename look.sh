8e654dfdef02492bbd7d15d46cae601719d99f78
Thomas H <116467716+Wemmy0@users.noreply.github.com>
2023-05-02 11:06:48 -0400

    Fix look.sh

--- look.sh
+++ look.sh
@@ -1,4 +1,10 @@
-sTudo dnf copr enable peterwu/rendezvous
-sudo dnf install bibata-cursor-themes
-dnf copr enable nickavem/adw-gtk3
-dnf install adw-gtk3
+# Bibata Cursor (https://github.com/ful1e5/Bibata_Cursor)
+sudo dnf copr enable peterwu/rendezvous -y
+sudo dnf install bibata-cursor-themes -y
+# adw-gtk3 - Awaita for legacy applications (https://github.com/lassekongo83/adw-gtk3)
+sudo dnf copr enable nickavem/adw-gtk3 -y
+sudo dnf install adw-gtk3 -y
+# Papirus Icons
+cd ~
+mkdir .icons
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
