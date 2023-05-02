0db1cfe6fe046b6dfa60dc61232cee9117fc7a4e
Thomas H <116467716+Wemmy0@users.noreply.github.com>
2023-05-02 11:19:05 -0400

    Update look.sh
    Add oh my zsh

--- look.sh
+++ look.sh
@@ -1,10 +1,12 @@
 # Bibata Cursor (https://github.com/ful1e5/Bibata_Cursor)
-sudo dnf copr enable peterwu/rendezvous -y
-sudo dnf install bibata-cursor-themes -y
 # adw-gtk3 - Awaita for legacy applications (https://github.com/lassekongo83/adw-gtk3)
-sudo dnf copr enable nickavem/adw-gtk3 -y
-sudo dnf install adw-gtk3 -y
+sudo dnf copr enable peterwu/rendezvous nickavem/adw-gtk3 -y
+sudo dnf install bibata-cursor-themes adw-gtk3 -y
 # Papirus Icons
 cd ~
 mkdir .icons
 wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
+# Oh my Zsh (https://ohmyz.sh/)
+sudo dnf install util-linux-user -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo dnf remove util-linux-user -y
