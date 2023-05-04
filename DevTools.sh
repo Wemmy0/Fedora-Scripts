f7d5268453ff9b4bd3e83ea33ffa967c2679e922
Tom H <116467716+Wemmy0@users.noreply.github.com>
2023-05-04 12:28:01 +0100

    Update DevTools.sh

--- DevTools.sh
+++ DevTools.sh
@@ -1,8 +1,9 @@
-sudo dnf install gcc -y
+# Install gcc for rustup
+# Install replacement for cat, bat (allows for syntax highlighting)
+sudo dnf install gcc bat -y
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# Install replacement for cat, bat (allows for syntax highlighting)
-cargo install bat
+sudo dnf install bat -y
 # Create an alias from cat to bat
 alias cat=bat
 echo "✅ Installed Rust, Bat and gcc"
