f4b96e4b9225ddb9abc5af67c98a740c1554cb19
Tom H <116467716+Wemmy0@users.noreply.github.com>
2023-05-09 20:08:56 +0100

    Update DevTools.sh

--- DevTools.sh
+++ DevTools.sh
@@ -1,9 +1,8 @@
 # Install gcc for rustup
 # Install replacement for cat, bat (allows for syntax highlighting)
-sudo dnf install gcc bat -y
+sudo dnf install gcc bat python3-devel -y
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo dnf install bat -y
 # Create an alias from cat to bat
 alias cat=bat
 echo "alias cat=bat" >> .bashrc
