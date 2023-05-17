0a51b260fdd9d008bda2c7305760579ad0595e95
Tom H <116467716+Wemmy0@users.noreply.github.com>
2023-05-17 17:10:39 +0100

    Fix Apperance.sh

--- Apperance.sh
+++ Apperance.sh
@@ -1,7 +1,7 @@
 # Bibata Cursor (https://github.com/ful1e5/Bibata_Cursor)
 # adw-gtk3 - Awaita for legacy applications (https://github.com/lassekongo83/adw-gtk3)
-sudo dnf copr enable peterwu/rendezvous nickavem/adw-gtk3 -y
-sudo dnf install bibata-cursor-themes adw-gtk3 -y
+sudo dnf copr enable peterwu/rendezvous -y
+sudo dnf install bibata-cursor-themes adw-gtk3-theme -y
 # Papirus Icons
 cd ~
 mkdir .icons
@@ -9,4 +9,3 @@ wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
 # Oh my Zsh (https://ohmyz.sh/)
 sudo dnf install util-linux-user -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo dnf remove util-linux-user -y
